1d5efaca0aeb647185490a740bbaf614dc8c27b1
Rasmus Söderhielm <72399173+laxsjo@users.noreply.github.com>
2025-10-14 16:13:26 +0200

    feat(backend): Unpin all slide groups at 05:00; Archive expired slide groups (#29)
    * chore(backend): Add scheduled tasks
    
    * feat(backend): Unpin all slide groups at 05:00
    
    Adds a background job which unpins all slide groups at 05:00 every day.
    
    Resolves #4
    
    * feat(backend): Archive expired slide groups
    
    Adds background job which archives all slide groups whose end dates
    expired at least a day ago. Currently the job runs once per minute.
    
    Resolves #5

---
+++ crates/backend/src/scheduler.rs
@@ -0,0 +1,87 @@
+//! Defines jobs which are ran at set intervals in the background.
+
+use chrono::Utc;
+use chrono_tz::Europe::Stockholm;
+use clokwerk::{AsyncScheduler, Job, TimeUnits};
+use entity::slide_group;
+use rocket::{tokio, Orbit, Rocket};
+use sea_orm::{ActiveValue, ColumnTrait, DatabaseConnection, DbErr, EntityTrait, QueryFilter};
+use sea_orm_rocket::Database;
+use std::{fmt::Display, future::Future, time::Duration};
+
+use crate::pool::Db;
+
+/// Handle result from async function, logging the error if it has failed without panicing.
+async fn log_job_error<O, E, R>(result: R)
+where
+    R: Future<Output = Result<O, E>>,
+    E: Display,
+{
+    if let Err(error) = result.await {
+        println!("Job generated error: {}", error);
+    }
+}
+
+pub async fn unpin_slide_groups(db: DatabaseConnection) -> Result<(), DbErr> {
+    println!("Unpinning slides groups");
+
+    slide_group::Entity::update_many()
+        .set(slide_group::ActiveModel {
+            priority: ActiveValue::Set(0),
+            ..Default::default()
+        })
+        .filter(slide_group::Column::ArchiveDate.is_null())
+        .exec(&db)
+        .await?;
+
+    Ok(())
+}
+
+/// Archive all slide groups with an end date which expired at least a day ago.
+pub async fn archive_expired_slide_groups(db: DatabaseConnection) -> Result<(), DbErr> {
+    let now = Utc::now().naive_utc();
+    let cutoff_date = now - chrono::Duration::days(1);
+
+    let result = slide_group::Entity::update_many()
+        .set(slide_group::ActiveModel {
+            archive_date: ActiveValue::Set(Some(now)),
+            ..Default::default()
+        })
+        .filter(slide_group::Column::ArchiveDate.is_null())
+        .filter(slide_group::Column::EndDate.lte(cutoff_date))
+        .exec(&db)
+        .await?;
+
+    if result.rows_affected > 0 {
+        println!("Archived {} expired slide groups", result.rows_affected);
+    }
+
+    Ok(())
+}
+
+pub async fn start(rocket: &Rocket<Orbit>) {
+    let mut scheduler = AsyncScheduler::with_tz(Stockholm);
+
+    let db = Db::fetch(rocket)
+        .expect("Rocket is in orbit phase")
+        .conn
+        .clone();
+
+    let cloned_db = db.clone();
+    scheduler
+        .every(1.days())
+        .at("05:00")
+        .run(move || log_job_error(unpin_slide_groups(cloned_db.clone())));
+
+    let cloned_db = db.clone();
+    scheduler
+        .every(1.minutes())
+        .run(move || log_job_error(archive_expired_slide_groups(cloned_db.clone())));
+
+    tokio::spawn(async move {
+        loop {
+            scheduler.run_pending().await;
+            tokio::time::sleep(Duration::from_millis(100)).await;
+        }
+    });
+}
