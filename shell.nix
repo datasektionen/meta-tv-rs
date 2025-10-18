7787a2585013f878ea2069802a104a9df1491e69
laxsjo <rasmus.soderhielm@gmail.com>
2025-10-18 14:58:10 +0200

    chore: Add missing libs to shell.nix
    Adds pkg-config and openssl to the Nix shell configuration. Not sure
    why it wasn't a problem before, but I'm not able to run `cargo check`
    without it.
    
    Also added some environment variables which I'm like 80 % are necessary.

--- shell.nix
+++ shell.nix
@@ -1,16 +1,33 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    cargo
-    rustc
-    rustfmt
-    rust-analyzer
-    clippy
-    sea-orm-cli
-
-    trunk
-    leptosfmt
-    lld
-    tailwindcss_4
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  libs = with pkgs; [
+    openssl
   ];
+  libPath = pkgs.lib.makeLibraryPath libs;
+in
+pkgs.mkShell {
+  packages =
+    with pkgs;
+    [
+      cargo
+      rustc
+      rustfmt
+      rust-analyzer
+      clippy
+      sea-orm-cli
+      pkg-config
+
+      trunk
+      leptosfmt
+      lld
+      tailwindcss_4
+    ]
+    ++ libs;
+  env = {
+    LD_LIBRARY_PATH = libPath;
+    # Required by rust-analyzer.
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  };
 }
