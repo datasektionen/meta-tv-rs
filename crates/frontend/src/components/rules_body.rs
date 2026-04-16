65a9556c6f9fd6d5a4f845f1aaf0215af63b6941
laxsjo <rasmus.soderhielm@gmail.com>
2026-04-16 19:44:15 +0200

    Add Generative AI policy + link to Media's policy
    Adds a link to Media's Code of Conduct, as well as the rule that images
    or video created with generative AI is forbidden. The rule is supposed
    to be added to Media's Policy for Officials, but it hasn't been updated
    yet, so I couldn't link to it.

--- crates/frontend/src/components/rules_body.rs
+++ crates/frontend/src/components/rules_body.rs
@@ -6,12 +6,21 @@ pub fn RulesBody() -> impl IntoView {
     view! {
         <ul
             role="list"
-            class="list-outside list-disc ps-[2ch] marker:content-['§_'] marker:font-bold marker:text-[1.2em] flex flex-col gap-1"
+            class="list-outside list-disc ps-[2ch] marker:content-['§_'] marker:font-bold marker:text-[1.2em] flex flex-col gap-1 max-w-[60ch]"
         >
             <li>
-                "Uploaded slide media must follow the chapters "
-                <a href="https://styrdokument.datasektionen.se/policies/uppforandepolicy">
-                    "Code of Conduct"
+                "Uploaded slide media must follow the "
+                <a
+                    class="link hover:text-current/80"
+                    href="https://styrdokument.datasektionen.se/policies/uppforandepolicy"
+                >
+                    "data chapter's Code of Conduct"
+                </a> " as well as the "
+                <a
+                    class="link hover:text-current/80"
+                    href="https://storage.googleapis.com/medieteknik-static/documents/2024/3/19/Uppförandepolicy%20EN.pdf"
+                >
+                    "media chapter's Code of Conduct"
                 </a> "."
             </li>
             <li>"Animated media must not contain blinking lights."</li>
@@ -20,6 +29,17 @@ pub fn RulesBody() -> impl IntoView {
                 <p class="ps-8 italic">
                     "This does not apply for slides only shown during an event."
                 </p>
+            </li>
+            <li>
+                "Per the "
+                <a
+                    class="link hover:text-current/80"
+                    href="https://styrdokument.datasektionen.se/pm/pm_informationsspridning#7-användning-av-generativ-ai"
+                >
+                    "data chapter's PM for Information Exchange"
+                </a>
+                " as well as the media chapter's Policy for Officials, slide media must not use "
+                "images or video created with generative AI."
             </li>
         </ul>
     }
